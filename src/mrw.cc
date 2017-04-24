cf12e62dc313a1cfba0a0a50adc6721df3169063
kuro <mhgeoe@gmail.com>
2017-04-24 18:18:10 +0900

    refactored

--- src/mrw.cc
+++ src/mrw.cc
@@ -48,15 +48,67 @@ inline void PrintNewHeuristicValue(int min_h, int g) {
             << generated << " generated]" << std::endl;
 }
 
+inline void PrintStopWalk(int i) {
+  std::cout << "Exploration stopped " << i << " random walks" << std::endl;
+}
+
+inline void PrintLengthWalk(int length_walk) {
+  std::cout << "New length of random walk: " << length_walk << std::endl;
+}
+
+inline bool UpdatePAP(int i, int h_min_old, int h_min, double *p, double *ap) {
+  if (h_min == -1)
+    *p = 0.0;
+  else
+    *p = std::max(0.0, static_cast<double>(h_min_old - h_min));
+  if (i == 0) *ap = *p;
+  bool result = *p > *ap;
+  *ap = (1.0-kAlpha)*(*ap) + kAlpha*(*p);
+  return result;
+}
+
+inline int ExtendLengthWalk(double extending_rate, int length_walk) {
+  return static_cast<int>(extending_rate * static_cast<double>(length_walk));
+}
+
+inline void UpdateMinimum(int h, vector<int> &s_prime, vector<int> &footprints,
+                          int *h_min, vector<int> &s_min,
+                          vector<int> &best_sequence) {
+  *h_min = h;
+  s_min = std::move(s_prime);
+  best_sequence = std::move(footprints);
+}
+
+inline void UpdateState(vector<int> &s_prime, vector<int> &footprints,
+                        vector<int> &s, vector<int> &sequence) {
+  s = std::move(s_prime);
+  sequence.insert(sequence.end(), footprints.begin(), footprints.end());
+}
+
+void UpdateMDA(int h, const vector<int> &footprints) {
+  if (h == -1) {
+    for (auto v : footprints)
+      faild[v] += 1;
+  } else {
+    for (auto v : footprints)
+      success[v] += 1;
+  }
+  for (int j=0, n=q_mda.size(); j<n; ++j) {
+    q_mda[j] = static_cast<double>(success[j] + faild[j]);
+    if (success[j] + faild[j] != 0)
+      q_mda[j] = 0.0 - (static_cast<double>(faild[j]) / q_mda[j]);
+  }
+}
+
 int PureRandomWalk(int h_min_old, const vector<int> &fact_offset,
                    const vector<var_value_t> &goal, const Actions &actions,
                    const TrieTable &table, vector<int> &s,
                    vector<int> &sequence) {
   int length_walk = kLengthWalk;
-  std::cout << "New length of random walk: " << length_walk << std::endl;
+  PrintLengthWalk(length_walk);
   int h_min = -1;
   vector<int> s_min;
-  std::vector<int> best_sequence;
+  vector<int> best_sequence;
   int counter = 0;
   double p, ap;
   std::random_device seed_gen;
@@ -65,6 +117,11 @@ int PureRandomWalk(int h_min_old, const vector<int> &fact_offset,
     ++total_walks;
     auto s_prime = s;
     vector<int> footprints;
+    if (counter > kExtendingPeriod) {
+      length_walk = ExtendLengthWalk(kExtendingRate, length_walk);
+      PrintLengthWalk(length_walk);
+      counter = 0;
+    }
     for (int j=0; j<length_walk; ++j) {
       auto a_set = FindFromTable(table, s_prime, fact_offset);
       if (a_set.empty()) {
@@ -79,8 +136,7 @@ int PureRandomWalk(int h_min_old, const vector<int> &fact_offset,
       footprints.push_back(a);
       ++generated;
       if (GoalCheck(goal, s_prime)) {
-        s = std::move(s_prime);
-        sequence.insert(sequence.end(), footprints.begin(), footprints.end());
+        UpdateState(s_prime, footprints, s, sequence);
         return 0;
       }
     }
@@ -90,59 +146,28 @@ int PureRandomWalk(int h_min_old, const vector<int> &fact_offset,
     ++evaluated;
     for (auto a : helpful_actions)
       q_mha[a] += 1.0;
-    if (h == -1) {
-      for (auto v : footprints)
-        faild[v] += 1;
-    } else {
-      for (auto v : footprints)
-        success[v] += 1;
-    }
-    for (int j=0, n=q_mda.size(); j<n; ++j) {
-      q_mda[j] = static_cast<double>(success[j] + faild[j]);
-      if (success[j] + faild[j] != 0)
-        q_mda[j] = 0.0 - (static_cast<double>(faild[j]) / q_mda[j]);
-    }
+    UpdateMDA(h, footprints);
     if ((h < h_min || h_min == -1) && h != -1) {
-      s_min = std::move(s_prime);
-      h_min = h;
-      best_sequence = std::move(footprints);
+      UpdateMinimum(h, s_prime, footprints, &h_min, s_min, best_sequence);
       counter = 0;
     } else {
       ++counter;
-      if (counter > kExtendingPeriod) {
-        length_walk = static_cast<int>(
-            kExtendingRate * static_cast<double>(length_walk));
-        counter = 0;
-        std::cout << "New length of random walk: " << length_walk << std::endl;
-      }
     }
-    if (h_min == -1)
-      p = 0.0;
-    else
-      p = std::max(0.0, static_cast<double>(h_min_old - h_min));
-    if (i == 0) ap = p;
-    if (p > ap) {
-      std::cout << "Exploration stopped " << i+1 << " random walks"
-                << std::endl;
-      s = std::move(s_min);
-      sequence.insert(sequence.end(), best_sequence.begin(),
-                      best_sequence.end());
-      return h_min;
-    }
-    ap = (1.0-kAlpha)*ap + kAlpha*p;
+    if (!UpdatePAP(i, h_min_old, h_min, &p, &ap)) continue;
+    PrintStopWalk(i+1);
+    UpdateState(s_min, best_sequence, s, sequence);
+    return h_min;
   }
-  std::cout << "Exploration stopped " << kNumWalk << " random walks"
-            << std::endl;
+  PrintStopWalk(kNumWalk);
   if (h_min == -1) return h_min_old;
-  s = std::move(s_min);
-  sequence.insert(sequence.end(), best_sequence.begin(), best_sequence.end());
+  UpdateState(s_min, best_sequence, s, sequence);
   return h_min;
 }
 
 int GibbsSampling(const vector<int> &v, const vector<double> &q, double tau,
                   double value) {
   int n = v.size();
-  std::vector<double> p(n);
+  vector<double> p(n);
   double sum = 0.0;
   for (int i=0; i<n; ++i) {
     p[i] = exp(q[v[i]]/tau);
@@ -160,10 +185,10 @@ int MDARandomWalk(int h_min_old, const vector<int> &fact_offset,
                   const TrieTable &table, vector<int> &s,
                   vector<int> &sequence) {
   int length_walk = kMDALengthWalk;
-  std::cout << "New length of random walk: " << length_walk << std::endl;
+  PrintLengthWalk(length_walk);
   int h_min = -1;
   vector<int> s_min;
-  std::vector<int> best_sequence;
+  vector<int> best_sequence;
   int counter = 0;
   double p, ap;
   std::random_device seed_gen;
@@ -172,6 +197,11 @@ int MDARandomWalk(int h_min_old, const vector<int> &fact_offset,
   for (int i=0; i<kNumWalk; ++i) {
     auto s_prime = s;
     vector<int> footprints;
+    if (counter > kExtendingPeriod) {
+      length_walk = ExtendLengthWalk(kMDAExtendingRate, length_walk);
+      PrintLengthWalk(length_walk);
+      counter = 0;
+    }
     for (int j=0; j<length_walk; ++j) {
       auto a_set = FindFromTable(table, s_prime, fact_offset);
       if (a_set.empty()) break;
@@ -180,8 +210,7 @@ int MDARandomWalk(int h_min_old, const vector<int> &fact_offset,
       footprints.push_back(a);
       ++generated;
       if (GoalCheck(goal, s_prime)) {
-        s = std::move(s_prime);
-        sequence.insert(sequence.end(), footprints.begin(), footprints.end());
+        UpdateState(s_prime, footprints, s, sequence);
         return 0;
       }
     }
@@ -189,52 +218,21 @@ int MDARandomWalk(int h_min_old, const vector<int> &fact_offset,
     int h = FF(s_prime, fact_offset, goal, actions, schema, &graph,
                helpful_actions);
     ++evaluated;
-    if (h == -1) {
-      for (auto v : footprints)
-        faild[v] += 1;
-    } else {
-      for (auto v : footprints)
-        success[v] += 1;
-    }
-    for (int j=0, n=q_mda.size(); j<n; ++j) {
-      q_mda[j] = static_cast<double>(success[j] + faild[j]);
-      if (success[j] + faild[j] != 0)
-        q_mda[j] = 0.0 - (static_cast<double>(faild[j]) / q_mda[j]);
-    }
+    UpdateMDA(h, footprints);
     if ((h < h_min || h_min == -1) && h != -1) {
-      s_min = std::move(s_prime);
-      h_min = h;
-      best_sequence = std::move(footprints);
+      UpdateMinimum(h, s_prime, footprints, &h_min, s_min, best_sequence);
       counter = 0;
     } else {
       ++counter;
-      if (counter > kExtendingPeriod) {
-        length_walk = static_cast<int>(
-            kMDAExtendingRate * static_cast<double>(length_walk));
-        counter = 0;
-        std::cout << "New length of random walk: " << length_walk << std::endl;
-      }
     }
-    if (h_min == -1)
-      p = 0.0;
-    else
-      p = std::max(0.0, static_cast<double>(h_min_old - h_min));
-    if (i == 0) ap = p;
-    if (p > ap) {
-      std::cout << "Exploration stopped " << i+1 << " random walks"
-                << std::endl;
-      s = std::move(s_min);
-      sequence.insert(sequence.end(), best_sequence.begin(),
-                      best_sequence.end());
-      return h_min;
-    }
-    ap = (1.0-kAlpha)*ap + kAlpha*p;
+    if (!UpdatePAP(i, h_min_old, h_min, &p, &ap)) continue;
+    PrintStopWalk(i+1);
+    UpdateState(s_min, best_sequence, s, sequence);
+    return h_min;
   }
-  std::cout << "Exploration stopped " << kNumWalk << " random walks"
-            << std::endl;
+  PrintStopWalk(kNumWalk);
   if (h_min == -1) return h_min_old;
-  s = std::move(s_min);
-  sequence.insert(sequence.end(), best_sequence.begin(), best_sequence.end());
+  UpdateState(s_min, best_sequence, s, sequence);
   return h_min;
 }
 
@@ -243,10 +241,10 @@ int MHARandomWalk(int h_min_old, const vector<int> &fact_offset,
                   const TrieTable &table, vector<int> &s,
                   vector<int> &sequence) {
   int length_walk = kLengthWalk;
-  std::cout << "New length of random walk: " << length_walk << std::endl;
+  PrintLengthWalk(length_walk);
   int h_min = -1;
   vector<int> s_min;
-  std::vector<int> best_sequence;
+  vector<int> best_sequence;
   int counter = 0;
   double p, ap;
   std::random_device seed_gen;
@@ -255,6 +253,11 @@ int MHARandomWalk(int h_min_old, const vector<int> &fact_offset,
   for (int i=0; i<kNumWalk; ++i) {
     auto s_prime = s;
     vector<int> footprints;
+    if (counter > kExtendingPeriod) {
+      length_walk = ExtendLengthWalk(kExtendingRate, length_walk);
+      PrintLengthWalk(length_walk);
+      counter = 0;
+    }
     for (int j=0; j<length_walk; ++j) {
       auto a_set = FindFromTable(table, s_prime, fact_offset);
       if (a_set.empty()) break;
@@ -263,8 +266,7 @@ int MHARandomWalk(int h_min_old, const vector<int> &fact_offset,
       footprints.push_back(a);
       ++generated;
       if (GoalCheck(goal, s_prime)) {
-        s = std::move(s_prime);
-        sequence.insert(sequence.end(), footprints.begin(), footprints.end());
+        UpdateState(s_prime, footprints, s, sequence);
         return 0;
       }
     }
@@ -275,39 +277,19 @@ int MHARandomWalk(int h_min_old, const vector<int> &fact_offset,
     for (auto a : helpful_actions)
       q_mha[a] += 1.0;
     if ((h < h_min || h_min == -1) && h != -1) {
-      s_min = std::move(s_prime);
-      h_min = h;
-      best_sequence = std::move(footprints);
+      UpdateMinimum(h, s_prime, footprints, &h_min, s_min, best_sequence);
       counter = 0;
     } else {
       ++counter;
-      if (counter > kExtendingPeriod) {
-        length_walk = static_cast<int>(
-            kExtendingRate * static_cast<double>(length_walk));
-        counter = 0;
-        std::cout << "New length of random walk: " << length_walk << std::endl;
-      }
     }
-    if (h_min == -1)
-      p = 0.0;
-    else
-      p = std::max(0.0, static_cast<double>(h_min_old - h_min));
-    if (i == 0) ap = p;
-    if (p > ap) {
-      std::cout << "Exploration stopped " << i+1 << " random walks"
-                << std::endl;
-      s = std::move(s_min);
-      sequence.insert(sequence.end(), best_sequence.begin(),
-                      best_sequence.end());
-      return h_min;
-    }
-    ap = (1.0-kAlpha)*ap + kAlpha*p;
+    if (!UpdatePAP(i, h_min_old, h_min, &p, &ap)) continue;
+    PrintStopWalk(i+1);
+    UpdateState(s_min, best_sequence, s, sequence);
+    return h_min;
   }
-  std::cout << "Exploration stopped " << kNumWalk << " random walks"
-            << std::endl;
+  PrintStopWalk(kNumWalk);
   if (h_min == -1) return h_min_old;
-  s = std::move(s_min);
-  sequence.insert(sequence.end(), best_sequence.begin(), best_sequence.end());
+  UpdateState(s_min, best_sequence, s, sequence);
   return h_min;
 }
 
@@ -326,7 +308,6 @@ vector<int> MRW(const vector<int> &initial, const vector<int> &fact_offset,
   faild.resize(n_actions);
   std::fill(faild.begin(), faild.end(), 0.0);
 
-  vector<int> sequence;
   vector<int> s = initial;
   ++generated;
   vector<int> helpful_actions;
@@ -334,7 +315,9 @@ vector<int> MRW(const vector<int> &initial, const vector<int> &fact_offset,
                          helpful_actions);
   ++evaluated;
   if (initial_h_min == -1) return vector<int>{-1};
+  vector<int> sequence;
   int h_min = initial_h_min;
+  PrintNewHeuristicValue(h_min, sequence.size());
   int counter = 0;
   double fail_rate = 0.0;
   double branching_factor = 0.0;
